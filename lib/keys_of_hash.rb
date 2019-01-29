8a4610aa611914cc3e257085d3c9e7e73778c89a
Oliver <oliver.grant@gmail.com>
2019-01-29 20:59:08 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+  def keys_of(*arguments)
+		animals = []
+		self.each do |animal, place|
+			if arguments.include?(place)
+				animals << animal
+			end
+		end
+		animals
+  end	
 end
