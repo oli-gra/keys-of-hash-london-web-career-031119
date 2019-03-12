2c835f838fd9638b98505b9faad9bae847f18982
Oliver <oliver.grant@gmail.com>
2019-03-12 05:51:13 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
   def keys_of(*arguments)
+<<<<<<< HEAD
 		animals = collect {|animal, place| arguments.include?(place) ? animal : nil}.compact!
+=======
+		animals = []
+		self.each do |animal, place|
+			if self.include?(place)
+				animals << animal
+			end
+		end
+		return animals
+>>>>>>> 89af5432132c06a9ea2398556b5c0e899d8c4813
   end	
 end
